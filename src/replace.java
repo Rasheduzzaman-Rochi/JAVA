ec3424c650414103f508dcafed4e45dbe9385c7c
Rasheduzzaman Rochi <rasheduzzaman2003@gmail.com>
2024-10-02 05:18:43 +0600

    New Files added

---
+++ src/replace.java
@@ -0,0 +1,7 @@
+public class replace {
+    public static void main (String[] args){
+        String name="Apid";
+        System.out.println(name.replace("p","b"));
+        System.out.println(name);
+    }
+}
---
+++ src/subString.java
@@ -0,0 +1,6 @@
+public class subString {
+    public static void main (String [] args){
+        String name ="IUB";
+        System.out.println(name.substring(1,3));
+    }
+}
