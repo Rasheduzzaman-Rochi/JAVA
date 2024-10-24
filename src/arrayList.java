5cd7007daf45a9dc5779ac5954fb3252e7d8f396
Rasheduzzaman Rochi <rasheduzzaman2003@gmail.com>
2024-10-24 16:00:18 +0600

    ArrayList updated

--- src/arrayList.java
+++ src/arrayList.java
@@ -1,16 +1,21 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class arrayList {
     public static void main(String[] args) {
         ArrayList<Integer> arrayList = new ArrayList<>();
         arrayList.add(1);
-        arrayList.add(2);
+        arrayList.add(5);
         arrayList.add(3);
 
         System.out.println(arrayList);
         System.out.println(arrayList.get(1));
 
         arrayList.set(2, 4);
+        System.out.println(arrayList);
+
+        Collections.sort(arrayList);
         System.out.println(arrayList);
     }
 }
